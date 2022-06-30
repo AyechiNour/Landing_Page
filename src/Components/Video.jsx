7da305fa374894b3618c072ac604865acb1a20dd
AyechiNour <83842252+AyechiNour@users.noreply.github.com>
2022-06-30 14:04:07 +0200

    Add files via upload

---
+++ src/Components/Video.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Video = () => {
+  return (
+    <>
+        <img className='border' src="/Images/Video.png" alt="" srcset="" />
+    </>
+  )
+}
+
+export default Video
